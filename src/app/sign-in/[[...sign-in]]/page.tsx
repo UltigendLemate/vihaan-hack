564597c4b0897920f640062243aaef67264f15cc
Chaitanya Anand <chaitanya.anand@gmail.com>
2024-04-13 02:47:05 +0530

    signin signup

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,8 @@
 import { SignIn } from "@clerk/nextjs";
  
 export default function Page() {
-  return <SignIn />;
+  return <div className="w-full h-screen flex justify-center items-center">
+    <SignIn />
+
+  </div>
 }
